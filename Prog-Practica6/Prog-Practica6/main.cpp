d7c8161020630eddd764733f50d8b9cbea5b18cf
Pakirra <bepitic@gmail.com>
2013-05-06 14:16:32 +0200

    asdzx

---
+++ Prog-Practica6/Prog-Practica6/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Prog-Practica6
+//
+//  Created by Paco Amoros on 06/05/13.
+//  Copyright (c) 2013 Paco Amoros. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
